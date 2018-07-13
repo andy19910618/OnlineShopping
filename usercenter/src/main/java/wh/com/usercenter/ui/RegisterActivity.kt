ab9fbd67ada68bfd89ae2a5a5b9034b597a2550f
wanghua <1614593285@qq.com>
2018-07-13 20:30:47 +0800

    引入anko

--- usercenter/src/main/java/wh/com/usercenter/ui/RegisterActivity.kt
+++ usercenter/src/main/java/wh/com/usercenter/ui/RegisterActivity.kt
@@ -2,6 +2,8 @@ package wh.com.usercenter.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.toast
 import wh.com.usercenter.R
 
 class RegisterActivity : AppCompatActivity() {
@@ -9,5 +11,11 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        btnHaveCode.setOnClickListener{
+//            Toast.makeText(this,"123456",Toast.LENGTH_SHORT).show()
+            toast("123456")
+//            startActivity(intentFor<TestActivity>("id" to 5))
+//            startActivity<TestActivity>("id" to 10)
+        }
     }
 }
